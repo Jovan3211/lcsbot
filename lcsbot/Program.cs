2dadb5f7d0168e7402ab26783dc318976af82a8d
Jovan <pady.soft@gmail.com>
2018-03-01 01:06:54 +0100

    Initial commit 2

---
+++ lcsbot/Program.cs
@@ -0,0 +1,101 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using tenebot;
+using tenebot.Services;
+
+namespace lcsbot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private Random rand = new Random();
+
+        public async Task RunBotAsync()
+        {
+            Settings._client = new DiscordSocketClient(Settings._config);
+            Settings._commands = new CommandService();
+
+            Settings._services = new ServiceCollection()
+                .AddSingleton(Settings._client)
+                .AddSingleton(Settings._commands)
+                .BuildServiceProvider();
+
+            Settings.LoadJson();
+            Settings.SqlServerSetup();
+
+            //subs
+            Settings._client.Log += Debugging.Log;
+            Settings._commands.Log += Debugging.Log;
+            Settings._client.MessageUpdated += MessageUpdated;
+            Settings._client.UserJoined += SqlHandler.TaskInsertUser;
+
+            await RegisterCommandAsync();
+            await Settings._client.LoginAsync(TokenType.Bot, Settings.BotToken);
+            await Settings._client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            Settings._client.MessageReceived += HandleCommandAsync;
+
+            await Settings._commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message == null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if ((message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(Settings._client.CurrentUser, ref argPos)) && !(message.Channel.ToString().Contains(message.Author.ToString())))
+            {
+                var context = new SocketCommandContext(Settings._client, message);
+                Debugging.Log("Command Handler", $"{context.User.Username} called {message}");
+
+                var result = await Settings._commands.ExecuteAsync(context, argPos, Settings._services);
+                if (!result.IsSuccess && result.Error != CommandError.ObjectNotFound || result.Error != CommandError.Exception || result.ErrorReason != "The server responded with error 400: BadRequest")
+                {
+                    Debugging.Log("Command Handler", $"Error with command {message}: {result.ErrorReason.Replace(".", "")}", LogSeverity.Warning);
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.WithTitle(":no_entry_sign:  Error!")
+                        .WithColor(Color.DarkRed)
+                        .WithDescription(result.ErrorReason);
+                    await arg.Channel.SendMessageAsync("", false, embed.Build());
+                }
+                else
+                {
+                    Debugging.Log("Command Handler", $"what", LogSeverity.Critical);
+                }
+            }
+            else if (message.Channel.ToString().Contains(message.Author.ToString())) //direct message
+            {
+                var context = new SocketCommandContext(Settings._client, message);
+                Debugging.Log("Command Handler, DM", $"{context.User.Username} sent {message}");
+
+                var result = await Settings._commands.ExecuteAsync(context, argPos, Settings._services);
+                if (!result.IsSuccess && result.Error != CommandError.ObjectNotFound || result.Error != CommandError.Exception)
+                {
+                    Debugging.Log("Command Handler, DM", $"Error with command {message}: {result.ErrorReason.Replace(".", "")}", LogSeverity.Warning);
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.WithTitle(":no_entry_sign:  DM Error!")
+                        .WithColor(Color.DarkRed)
+                        .WithDescription(result.ErrorReason);
+                    await arg.Channel.SendMessageAsync("", false, embed.Build());
+                }
+            }
+        }
+
+        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            var message = await before.GetOrDownloadAsync();
+        }
+    }
+}
---
+++ lcsbot/Services/ImageHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace tenebot.Services
+{
+    public static class ImageHandler
+    {
+        private static string imageBaseUrl = Settings.BaseHostUrl;
+
+        /// <summary>
+        /// Returns a random image from the server with a proper url path, also checks if its a jpg, png or gif.
+        /// </summary>
+        /// <param name="folder">Folder on the server containing number named images.</param>
+        /// <param name="max">Maximum random number result.</param>
+        /// <param name="min">Minimum random number result.</param>
+        /// <returns>Verified image url.</returns>
+        public static string RandomImageUrl(string folder, int max, int min = 1)
+        {
+            Random rnd = new Random();
+            int selected = rnd.Next(min, max);
+
+            string url = imageBaseUrl + folder + selected.ToString();
+
+            if (Debugging.CheckHttpReachable(url + ".jpg"))
+                return url + ".jpg";
+            else if (Debugging.CheckHttpReachable(url + ".png"))
+                return url + ".png";
+            else if (Debugging.CheckHttpReachable(url + ".gif"))
+                return url + ".gif";
+            else
+            {
+                Debugging.Log("RandomImageUrl", "Failed to find image url");
+                return "Failed to get url";
+            }
+        }
+    }
+}
