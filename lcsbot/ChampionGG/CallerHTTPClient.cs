20f72234f7d57ec29b00aa428e611d25d250427c
Jovan <pady.soft@gmail.com>
2018-03-01 19:42:27 +0100

    Started work on ChampGGAPI, got callerhttpclient for now

---
+++ lcsbot/ChampionGG/CallerHTTPClient.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+
+namespace lcsbot.ChampionGG
+{
+    public class CallerHTTPClient
+    {
+        private const string ChampionGGBaseUrl = "http://api.champion.gg/";
+
+        public static async Task<T> CallChampionGgApiAsync<T>(string apiCallPath)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ChampionGGBaseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var completeUrl = $"{ChampionGGBaseUrl}{apiCallPath}";
+                var response = await client.GetAsync(completeUrl);
+
+                var json = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<T>(json);
+                }
+
+                throw Exception.CreateForErrorCode((int)response.StatusCode);
+            }
+        }
+
+    }
+}
---
+++ lcsbot/ChampionGG/Exception.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lcsbot.ChampionGG
+{
+    public class Exception : System.Exception
+    {
+        public ChampionGgErrorCode Code { get; set; }
+
+        public Exception() { }
+
+        public Exception(string message) : base(message) { }
+
+        public Exception(string message, System.Exception innerException) : base(message, innerException) { }
+
+        public static Exception CreateForErrorCode(int code)
+        {
+            var errorCode = (ChampionGgErrorCode)code;
+            var msg = $"{(int)code}: {code}";
+            return new Exception(msg) { Code = errorCode };
+        }
+    }
+
+    public enum ChampionGgErrorCode
+    {
+        JsonSerializationError = 0,
+        BadRequest = 400,
+        Forbidden = 403,
+        DataNotFound = 404,
+        ServerError = 500,
+        Unavailable = 503
+    }
+}
