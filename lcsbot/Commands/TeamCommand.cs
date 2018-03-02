b7dec241b7658fb4a1338dfa4a9b50b22936e23b
Jovan <pady.soft@gmail.com>
2018-03-02 02:33:32 +0100

    idk

--- lcsbot/Commands/TeamCommand.cs
+++ lcsbot/Commands/TeamCommand.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Discord.Commands;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using lcsbot.Services;
 using Discord;
+using RiotSharp;
+using lcsbot.Riot;
+using System;
+using System.Linq;
 
 namespace lcsbot.Commands
 {
@@ -15,6 +14,9 @@ namespace lcsbot.Commands
     public class TeamCommand : ModuleBase<SocketCommandContext>
     {
         private List<string> teamPlayerIds = new List<string>();
+        private List<string> teamChampionIds = new List<string>();
+        private List<string> teamRoleIds = new List<string>();
+
         private bool saved = false;
 
         [Command("")]
@@ -36,22 +38,71 @@ namespace lcsbot.Commands
                 savedMessage = "Team is not saved, use `team save` when done.";
 
             MessageHandler messageHandler = new MessageHandler();
-            EmbedBuilder message = messageHandler.BuildEmbed("Your current team setup: ", savedMessage, Palette.Pink, teamPlayerIds, GetNamesForPlayers());
+            EmbedBuilder message = messageHandler.BuildEmbed("Your current team setup: ", savedMessage, Palette.Pink, GetNamesForSummoners(), GetNamesForSummoners());
 
             await ReplyAsync("", false, message.Build());
         }
 
-        private List<string> GetNamesForPlayers()
+        [Command("addplayer")]
+        public async Task AddPlayer(string summoner, string champion)
+        {
+            string savedMessage = "";
+
+
+
+            MessageHandler messageHandler = new MessageHandler();
+            EmbedBuilder message = messageHandler.BuildEmbed("Added player to your team", "", Palette.Pink);
+
+            await ReplyAsync("", false, message.Build());
+        }
+
+        private List<string> GetNamesForSummoners()
         {
             List<string> playerNames = new List<string>();
 
-            foreach (string player in teamPlayerIds)
+            foreach (string playerid in teamPlayerIds)
             {
-                //get username from riotapi by summoner id
-                playerNames.Add(player);
+                try
+                {
+                    playerNames.Add(RiotAPI.api.GetSummonerByAccountId(RiotSharp.Misc.Region.global, long.Parse(playerid)).Name);
+                }
+                catch (Exception e)
+                {
+                    Debugging.Log("GetNamesForSummoners", $"Error getting summoner and/or adding to list: {e.Message}");
+                }
             }
 
             return playerNames;
+        }
+
+        private List<string> GetNamesForChampions()
+        {
+            List<string> playerNames = new List<string>();
+
+            foreach (string playerid in teamPlayerIds)
+            {
+                try
+                {
+                    playerNames.Add(GetChampionByName("asd"));
+                }
+                catch (Exception e)
+                {
+                    Debugging.Log("GetNamesForChampions", $"Error getting champion and/or adding to list: {e.Message}");
+                }
+            }
+
+            return playerNames;
+        }
+
+        private RiotSharp.StaticDataEndpoint.Champion.ChampionStatic GetChampionByName(string name)
+        {
+            var champions = RiotAPI.staticapi.GetChampions(RiotSharp.Misc.Region.global).Champions;
+
+            foreach (RiotSharp.StaticDataEndpoint.Champion.ChampionStatic champ in champions)
+            {
+                if (champ.Name == name)
+                    return champ;
+            }
         }
     }
 }
---
+++ lcsbot/RiotAPI/RiotAPI.cs
@@ -0,0 +1,14 @@
+﻿using RiotSharp;
+using lcsbot.Services;
+
+namespace lcsbot.Riot
+{
+    public static class RiotAPI
+    {
+        /// <summary>
+        /// RiotApi Instance.
+        /// </summary>
+        public static RiotApi api = RiotApi.GetDevelopmentInstance(Settings.RiotAPIKey);
+        public static StaticRiotApi staticapi = StaticRiotApi.GetInstance(Settings.RiotAPIKey);
+    }
+}
